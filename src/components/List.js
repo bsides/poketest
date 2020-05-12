99290c1d9fc4f8fe59e045cf8d3fcea344ccc9c4
Rafael @bsides Pereira <rafaelbsides@gmail.com>
2020-05-11 22:24:02 -0300

    Changed to datatable

--- src/components/List.js
+++ src/components/List.js
@@ -5,14 +5,21 @@ import Details from './Details'
 import { makeStyles } from '@material-ui/core/styles'
 import Table from '@material-ui/core/Table'
 import TableBody from '@material-ui/core/TableBody'
-import TableCell from '@material-ui/core/TableCell'
 import TableContainer from '@material-ui/core/TableContainer'
 import TableHead from '@material-ui/core/TableHead'
+import TableCell from '@material-ui/core/TableCell'
 import TableRow from '@material-ui/core/TableRow'
 import Paper from '@material-ui/core/Paper'
 
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650,
+  },
+})
+
 const List = ({ url, nextUrl, previousUrl }) => {
   const { data: pokemonsReq, error } = useSWR(url, fetcher)
+  const classes = useStyles()
 
   if (error) return <p>Falhou ao tentar acessar a API.</p>
   if (!pokemonsReq) return <p>Carregando lista de pokemons...</p>
@@ -20,25 +27,42 @@ const List = ({ url, nextUrl, previousUrl }) => {
   const { results: pokemons, next, previous } = pokemonsReq.data
   if (!pokemons) return <p>Sem dados!</p>
 
+  // PS: this is a callback to update pagination and will
+  // trigger a warning from debugger. It's ok to ignore the
+  // warning as it's an intended action.
   nextUrl(next)
   previousUrl(previous)
 
   return (
-    <div>
-      <ul>
-        <li>
-          <p></p>
-          <p>#</p>
-          <p>Pokémon</p>
-          <p>Type</p>
-          <p>Ability</p>
-          <p>Form</p>
-        </li>
-        {pokemons.map((pokemon) => (
-          <Details url={pokemon.url} name={pokemon.name} key={pokemon.url} />
-        ))}
-      </ul>
-    </div>
+    <>
+      <TableContainer component={Paper}>
+        <Table
+          stickyHeader
+          aria-label="table of pokemons"
+          className={classes.table}
+        >
+          <TableHead>
+            <TableRow>
+              <TableCell>Sprite</TableCell>
+              <TableCell>#</TableCell>
+              <TableCell>Pokémon</TableCell>
+              <TableCell>Types</TableCell>
+              <TableCell>Abilities</TableCell>
+              <TableCell>Forms</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody stripedRows>
+            {pokemons.map((pokemon) => (
+              <Details
+                url={pokemon.url}
+                name={pokemon.name}
+                key={pokemon.url}
+              />
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
   )
 }
 
